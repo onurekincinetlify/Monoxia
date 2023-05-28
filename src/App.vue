0cee2b005b028452a2a539975d8fcc8e685b5320
onurekinciii <cve201916920@gmail.com>
2023-05-28 16:08:10 +0300

    Updated at 28-05-2023

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div>
+  <div v-if="userAgent.match(/Android/i) || userAgent.match(/webOS/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPad/i) || userAgent.match(/iPod/i) || userAgent.match(/BlackBerry/i) || userAgent.match(/Windows Phone/i)">
+    <div class="mobile-alert">
+      <Navbar/>
+      <h1>Our website currently does not have mobile support, so to avoid many errors and incompatibilities, our website is currently closed for mobile devices. Please try accessing it from a computer.</h1>
+    </div>
+  </div>
+  <div v-else>
     <Navbar/>
     <router-view key="route.fullPath"></router-view>
   </div>
@@ -14,7 +20,20 @@ import card from '/src/components/Cards.vue';
 
 const route = useRoute();
 const router = useRouter();
+
+var userAgent = navigator.userAgent;
+
+if (userAgent.match(/Android/i) || userAgent.match(/webOS/i) || userAgent.match(/iPhone/i) || userAgent.match(/iPad/i) || userAgent.match(/iPod/i) || userAgent.match(/BlackBerry/i) || userAgent.match(/Windows Phone/i)) {
+  console.log("Mobil cihaz");
+} else {
+  console.log("Masaüstü cihaz");
+}
 </script>
 
 <style lang="scss">
+.mobile-alert h1{
+  font-size: 20px;
+  text-align: center;
+  margin-top: 150px;
+}
 </style>
