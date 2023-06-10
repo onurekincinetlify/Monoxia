15ae283c31a147ce5ffdfcb125d42b1e365d01c7
onurekinciii <cve201916920@gmail.com>
2023-06-10 19:45:05 +0300

    Updated at 10-06-2023

--- src/components/Dashboard/Navbar/Formulas.vue
+++ src/components/Dashboard/Navbar/Formulas.vue
@@ -1,10 +1,65 @@
 <template>
-    <div class="settings">
-        <h1>Formulas Part</h1>
+    <div class="formulas">
+        <table class="table">
+  <thead>
+    <tr>
+      <th><abbr title="Position">Pos</abbr></th>
+      <th>Subject</th>
+      <th><abbr title="Subheading">SH</abbr></th>
+      <th><abbr title="Sub-Subheading">SSH</abbr></th>
+      <th>Formula</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>1</th>
+      <td>Demand</td>
+      <td>Demand Function</td>
+      <td></td>
+      <td>Mtx = f(Fx, Fr, Ft, G, T)</td>
+    </tr>
+    <tr>
+      <th>2</th>
+      <td>Demand</td>
+      <td>Demand Schedule and Demand Curve</td>
+      <td></td>
+      <td>Mtx = f(Fx)</td>
+    </tr>
+    <tr>
+      <th>3</th>
+      <td>Demand</td>
+      <td>Individual Demand Function and Market Demand Function</td>
+      <td></td>
+      <td>Mtx = f(Fx, Fr, Ft, G, T, N, Gd, K)</td>
+    </tr>
+    <tr>
+      <th>4</th>
+      <td>Demand</td>
+      <td>Price Elasticity of Demand</td>
+      <td></td>
+      <td>ef = (Percentage Change in Quantity Demanded / Percentage Change in Price)</td>
+    </tr>
+    <tr>
+      <th>5</th>
+      <td>Demand</td>
+      <td>Price Elasticity of Demand</td>
+      <td></td>
+      <td>er = (ΔM / M) / (ΔF / F) = (ΔM / ΔF) x F / M</td>
+    </tr>
+    <tr>
+      <th>6</th>
+      <td>Demand</td>
+      <td>Price Elasticity of Demand</td>
+      <td></td>
+      <td>ep = ((M1 - M2) / ((M1 + M2)/2)) / ((F1 - F2)/((F1 + F2)/2)) = (ΔM / (M1 + M2)) / (ΔF / F1 + F2)</td>
+    </tr>
+  </tbody>
+</table>
     </div>
 </template>
 
 <script lang="ts" setup>
+
 </script>
 
 <style scoped lang="scss"></style>
